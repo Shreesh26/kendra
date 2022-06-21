bc7ca2b053ddd135828c6a02c8843848c3a0103b
Shreesh26 <72539096+Shreesh26@users.noreply.github.com>
2022-06-21 11:56:25 +0530

    encoding file text

--- nlp/nlp.py
+++ nlp/nlp.py
@@ -1,5 +1,6 @@
 from datasets import Summarization
 from transformers import pipeline
+from transformers import AutoTokenizer
 
 def associatedLabel (sentence, label_list):
     classifier=pipeline("zero-shot-classification")
@@ -9,3 +10,10 @@ def associatedLabel (sentence, label_list):
 def document_summary(text):
     summarizer=pipeline("summarization")
     return summarizer(text)
+
+def document_encoder(text):
+    checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    raw_input=[text]
+    inputs = tokenizer(raw_input, padding=True, truncation=True, return_tensors="tf")
+    return inputs
